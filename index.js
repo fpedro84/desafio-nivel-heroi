db6fcf0a2f3f75106a0368d10c5e3157d789675f
fpedro84 <mfpbtu@gmail.com>
2024-11-27 12:17:38 -0300

    commit desafio nivel do herói

---
+++ index.js
@@ -0,0 +1,34 @@
+// Variáveis
+let nome = prompt("Digite o nome do herói:");
+let xp = parseInt(prompt("Digite a quantidade de experiência (XP) do herói:"));
+
+// Nível do herói de acordo com o XP
+let nivel;
+switch (true) {
+    case (xp < 1000):
+        nivel = "Ferro";
+        break;
+    case (xp >= 1001 && xp <= 2000):
+        nivel = "Bronze";
+        break;
+    case (xp >= 2001 && xp <= 5000):
+        nivel = "Prata";
+        break;
+    case (xp >= 5001 && xp <= 7000):
+        nivel = "Ouro";
+        break;
+    case (xp >= 7001 && xp <= 8000):
+        nivel = "Platina";
+        break;
+    case (xp >= 8001 && xp <= 9000):
+        nivel = "Ascendente";
+        break;
+    case (xp >= 9001 && xp <= 10000):
+        nivel = "Imortal";
+        break;
+    default:
+        nivel = "Radiante";
+}
+
+// Print da mensagem do herói
+console.log(`O Herói de nome ${nome} está no nível de ${nivel}`);
